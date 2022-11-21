26986c189d81a6bf085d06c9176d1854a97bdf0c
Nik181 <N_bolliger45@hotmail.com>
2022-11-21 16:56:48 +0100

    added const var for boxblur checked save routine

--- src/util/Util.java
+++ src/util/Util.java
@@ -6,9 +6,3 @@ public class Util {
     public static final BoxBlur BLURRFACTOR = new BoxBlur(2,2,4) ;
 }
 
-/**
- * TODO: Add BlurrFactor to all stages that open from inireader controller
- *
- * TODO: check file save process
- */
-
